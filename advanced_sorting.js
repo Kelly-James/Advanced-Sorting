14b9c387503d2edef83e5042327235849a3bd716
Kelly James Clancy <kellyjamesclancy@gmail.com>
2016-11-03 21:18:48 +0000

    Sorting function added

---
+++ advanced_sorting.js
@@ -0,0 +1,19 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+var sorted = students.sort(function(a, b) {
+  if(a.name === b.name) {
+    if (b.age > a.age) {
+      return 1;
+    }
+  } else if(b.name < a.name) {
+    return 1;
+  }
+});
+
+
+console.log(sorted);
